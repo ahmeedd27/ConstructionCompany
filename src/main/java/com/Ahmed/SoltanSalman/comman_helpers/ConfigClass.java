7f5f062df00958587ce0d55613eb8beec0dbd03b
ahmeed27 <ahmdbrl811@gmail.com>
2025-06-23 17:42:54 +0300

    updates

--- src/main/java/com/Ahmed/SoltanSalman/comman_helpers/ConfigClass.java
+++ src/main/java/com/Ahmed/SoltanSalman/comman_helpers/ConfigClass.java
@@ -1,5 +1,6 @@
 package com.Ahmed.SoltanSalman.comman_helpers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +13,11 @@ import java.util.Properties;
 import java.util.Random;
 
 @Configuration
+@RequiredArgsConstructor
 public class ConfigClass {
 
     @Value("${spring.mail.password}")
-    private String password;
+    private final String password;
 
     @Bean
     public JavaMailSender javaMailSender() {
